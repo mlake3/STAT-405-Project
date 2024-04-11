e3f282151f830e34fb5e3c2bbfc66d4edb6fc945
AHSAN Asyraf <ahsan@marzano13.stat.wisc.edu>
2024-04-11 10:26:42 -0500

    Updated preSetup script

--- preSetup.sh
+++ preSetup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-# This shell script automates downloading the archive.zip online.
+# This shell script automates downloading the archive.zip online, and then sets up the file for the project.
 wget -O archive.zip https://pages.stat.wisc.edu/~jwvanzeeland/archive.zip
+
+unzip archive.zip
+
+rm archive.zip
+
+mkdir data
+
+mv *202[23] data # move all the monthly directories into one file
+

a1de37ee749bac9c432dca2da49d2c28a4fd583e
Jack-Van-Zeeland <98611447+Jack-Van-Zeeland@users.noreply.github.com>
2024-04-26 08:01:27 -0500

    Create exchangeanalysis.sh
    runs exchange analysis.R

---
+++ exchangeanalysis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+module load R/R-3.6.1
+
+
+# define array of exchanges to be used with task ID (19)
+exchanges=("AUDJPY" "AUDNZD" "AUDUSD" "CADJPY" "CHFJPY" "EURCHF" "EURGBP" "EURJPY" "EURPLN" "EURUSD" "GBPJPY" "GBPUSD" "NZDUSD" "USDCAD" "USDCHF" "USDJPY" "USDMXN" "USDTRY" "USDZAR")
+
+n=$SLURM_ARRAY_TASK_ID
+index=$(($n-1))
+
+exchange=${exchanges[$index]}
+echo $exchange
+
+Rscript exchangeanalysis.R allexchanges/$exchange.csv

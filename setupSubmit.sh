be739012225b49dc5fb5e70a69d6f45ea499e949
ahsan <asyraf@wisc.edu>
2024-04-27 17:56:50 -0500

    updated dag process

--- setupSubmit.sh
+++ setupSubmit.sh
@@ -19,20 +19,63 @@ jobId2=$(sbatch --array=1-19 \
 jobId2=$(echo $jobId2 | sed 's/Submitted batch job //')
 
 # Install required packages
-jobId3=$(sbatch --array=1-5 \
+jobId3=$(sbatch --array=1-4 \
                 --output="slurm_out/slurm-%A_%a.out" \
                 --error="slurm_out/slurm-%A_%a.err" \
                 --dependency=afterok:$jobId2 \
                 installPackages.sh)
 jobId3=$(echo $jobId3 | sed 's/Submitted batch job //')             
 
+# Exchange analysis jobs (divided into 5 parts to increase efficiency of computer power)
 
-#Run exchange analysis in parrallel
-jobId4=$(sbatch --array=1-19 \
+## light job
+jobId4_1=$(sbatch --array=18 \
+		--cpus-per-task=1 \
                 --output="slurm_out/slurm-%A_%a.out" \
                 --error="slurm_out/slurm-%A_%a.err" \
+		--mem-per-cpu=1500M \
                 --dependency=afterok:$jobId3 \
                 exchangeanalysis.sh)
-jobId4=$(echo $jobId4 | sed 's/Submitted batch job //')             
+jobId4_1=$(echo $jobId4_1 | sed 's/Submitted batch job //')             
 
-#run final analysis
+## light-medium job
+jobId4_2=$(sbatch --array=4-5,9 \
+		--cpus-per-task=8 \
+                --output="slurm_out/slurm-%A_%a.out" \
+                --error="slurm_out/slurm-%A_%a.err" \
+		--mem-per-cpu=2600M \
+                --dependency=afterok:$jobId3 \
+                exchangeanalysis.sh)
+jobId4_2=$(echo $jobId4_2 | sed 's/Submitted batch job //')             
+
+## heavy-medium job
+jobId4_3=$(sbatch --array=2,3,6,7,13-15,17,19 \
+		--cpus-per-task=20 \
+                --output="slurm_out/slurm-%A_%a.out" \
+                --error="slurm_out/slurm-%A_%a.err" \
+		--mem-per-cpu=2600M \
+                --dependency=afterok:$jobId3 \
+                exchangeanalysis.sh)
+jobId4_3=$(echo $jobId4_3 | sed 's/Submitted batch job //')             
+
+
+
+## heavy job
+jobId4_4=$(sbatch --array=8,10,12,16 \
+		--cpus-per-task=30 \
+                --output="slurm_out/slurm-%A_%a.out" \
+                --error="slurm_out/slurm-%A_%a.err" \
+		--mem-per-cpu=2600M \
+                --dependency=afterok:$jobId3 \
+                exchangeanalysis.sh)
+jobId4_4=$(echo $jobId4_4 | sed 's/Submitted batch job //')             
+
+## super heavy job
+jobId4_5=$(sbatch --array=1,11 \
+		--cpus-per-task=40 \
+                --output="slurm_out/slurm-%A_%a.out" \
+                --error="slurm_out/slurm-%A_%a.err" \
+		--mem-per-cpu=2600M \
+                --dependency=afterok:$jobId3 \
+                exchangeanalysis.sh)
+jobId4_5=$(echo $jobId4_5 | sed 's/Submitted batch job //')             

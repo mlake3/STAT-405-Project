cea0f8a10546632566728f2b5797621561d463d2
Jack-Van-Zeeland <98611447+Jack-Van-Zeeland@users.noreply.github.com>
2024-05-04 18:54:57 -0500

    Update summaryanalysis.R

--- summaryanalysis.R
+++ summaryanalysis.R
@@ -8,7 +8,14 @@ if (length(args) == 1){
 
 .libPaths(new=c(RpackagesDir, .libPaths()))
 
+library(tidyverse)
+library(stringr)
+library(dplyr)
 library(ggplot2)
+library(stringr)
+library(tsibble)
+library(feasts)
+library(lubridate)
 
 #Read in summary data 
 summary_stats=read.csv("results/summary.csv")
@@ -46,3 +53,69 @@ ggplot(summary_stats, aes(x = exchanges, y = spread_sd/spread_mean, fill = slope
   ylab("Correlation Coefficient") +
   xlab("Exchange") 
 ggsave("SpreadCorrelationCoefficient.png")
+
+#Get coefficients & prediction
+lmresults = read_csv("results/lmresults.csv")
+max_row_indices <- c()
+
+# Loop through each column
+for (col_index in seq_along(lmresults)) {
+  # Find row index of maximum value in the current column
+  max_row_index <- which.max(lmresults[[col_index]])
+  max_row_indices = c(max_row_indices, max_row_index)
+  
+}
+
+
+test_data = read.csv("results/dailydata.csv")
+test_data = test_data %>% 
+  mutate(date = as.Date(date)) %>% 
+  arrange(exchange, date) 
+test_data = test_data %>%
+  group_by(exchange) %>%
+  mutate(slope = signif(as.double(coef(lm(market_daily ~ date))[2]), 3)) %>% ungroup()
+
+test_data_ts = as_tsibble(test_data, index = date, key = exchange)
+
+test_trends = test_data_ts %>%
+  group_by(exchange) %>%
+  features(market_daily, features = feature_set(tags = "trend")) %>%
+  ungroup() %>%
+  select(exchange, trend_strength, spikiness, linearity, curvature) %>%
+  mutate(
+    trend_strength = signif(trend_strength, 3), 
+    linearity = signif(linearity, 3), 
+    curvature = signif(curvature, 3), 
+    spikiness = signif(spikiness, 3)
+  )
+
+test_data = left_join(test_data, test_trends, by = "exchange")
+
+sum_test_data = test_data %>% group_by(exchange) %>% summarise(market_daily = mean(market_daily), slope = mean(slope), trend_strength = mean(trend_strength), spikiness = mean(spikiness))
+
+sum_test_data = sum_test_data %>% mutate(norm_slope = slope/market_daily)
+
+
+sum_test_data$indice = max_row_indices
+
+mean_slope = mean(sum_test_data$norm_slope)
+sd_slope = sd(sum_test_data$norm_slope)
+
+sum_test_data = sum_test_data %>% mutate(indice = as.numeric(indice),z_slope = (norm_slope - mean_slope)/sd_slope)
+
+test_lm = lm(data = sum_test_data, indice ~ z_slope:trend_strength + z_slope + trend_strength + spikiness)
+summary(test_lm)
+
+intercept = as.numeric(test_lm$coefficients[1])
+z_slope_coeff = as.numeric(test_lm$coefficients[2])  
+ts_coeff = as.numeric(test_lm$coefficients[3])  
+spikiness_coeff = as.numeric(test_lm$coefficients[4]) 
+z_ts_coeff = as.numeric(test_lm$coefficients[5])  
+
+sum_test_data = sum_test_data %>% mutate(score = intercept + z_slope_coeff*z_slope + ts_coeff*trend_strength + spikiness_coeff*spikiness + z_ts_coeff*z_slope*trend_strength, prediction = ifelse((score > 3 & indice >= 3) | (score < 3 & indice <= 3), "correct", "wrong"))
+sum(sum_test_data$prediction == "correct")
+
+
+
+
+sum_test_data %>% select(exchange, score, indice, prediction) %>% rename(optimal_exchanges = indice)
